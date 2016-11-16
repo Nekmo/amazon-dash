b2bd82b866627fb4d19c20eec5af5ba4a893d0eb
Nekmo <contacto@nekmo.com>
2016-11-16 23:29:26 +0100

    Execution and prevent duplicate macs.

--- amazon_dash/listener.py
+++ amazon_dash/listener.py
@@ -16,12 +16,18 @@ class Listener(object):
         self.config = Config(config_path)
         self.settings = self.config.get('settings', {})
         self.mac_devices = list(map(str.lower, self.config['devices'].keys()))
+        assert len(self.mac_devices) == len(self.config['devices']), "Duplicate(s) MAC(s) on devices config."
 
     def on_push(self, device):
         if last_execution[device.src] + self.settings.get('delay', DEFAULT_DELAY) > time.time():
             return
         last_execution[device.src] = time.time()
+        self.execute(device)
         print_pkt(device)
+
+    def execute(self, device):
+        devices = self.config.get('devices', {})
+        devices.get()
 
     def run(self):
         scan(self.on_push, lambda d: d.src.lower() in self.mac_devices)
