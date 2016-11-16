c3f7d89dac9c96277ad98d8fcf561dd6d8ee5e36
Nekmo <contacto@nekmo.com>
2016-11-16 23:16:26 +0100

    Execution delay.

--- amazon_dash/listener.py
+++ amazon_dash/listener.py
@@ -1,13 +1,24 @@
+from collections import defaultdict
+
+import time
+
 from amazon_dash.config import Config
 from amazon_dash.scan import scan, print_pkt
+
+
+last_execution = defaultdict(lambda: 0)
 
 
 class Listener(object):
     def __init__(self, config_path):
         self.config = Config(config_path)
+        self.settings = self.config.get('settings', {})
         self.mac_devices = list(map(str.lower, self.config['devices'].keys()))
 
     def on_push(self, device):
+        if last_execution[device.src] + self.settings.get('delay') > time.time():
+            return
+        last_execution[device.src] = time.time()
         print_pkt(device)
 
     def run(self):
