55fbb6750f3aedb544f6c84dab6043c8b1bc36a6
Nekmo <contacto@nekmo.com>
2016-11-16 23:21:51 +0100

    Default delay.

--- amazon_dash/listener.py
+++ amazon_dash/listener.py
@@ -9,6 +9,8 @@ from amazon_dash.scan import scan, print_pkt
 last_execution = defaultdict(lambda: 0)
 
 
+DEFAULT_DELAY = 10
+
 class Listener(object):
     def __init__(self, config_path):
         self.config = Config(config_path)
@@ -16,7 +18,7 @@ class Listener(object):
         self.mac_devices = list(map(str.lower, self.config['devices'].keys()))
 
     def on_push(self, device):
-        if last_execution[device.src] + self.settings.get('delay') > time.time():
+        if last_execution[device.src] + self.settings.get('delay', 10) > time.time():
             return
         last_execution[device.src] = time.time()
         print_pkt(device)
