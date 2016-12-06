daa12c3fb419f9dfb42bd381b731c6daecba6c49
Nekmo <contacto@nekmo.com>
2016-12-06 04:19:59 +0100

    Config file must not have write permissions.

--- amazon_dash/config.py
+++ amazon_dash/config.py
@@ -1,3 +1,5 @@
+import os
+import stat
 from yaml import load, dump
 try:
     from yaml import CLoader as Loader, CDumper as Dumper
@@ -5,9 +7,42 @@ except ImportError:
     from yaml import Loader, Dumper
 
 
+class SecurityException(Exception):
+    pass
+
+
+def bitperm(s, perm, pos):
+    perm = perm.lower()
+    pos = pos.lower()
+    assert perm in ['R', 'W', 'X']
+    assert pos in ['USR', 'GRP', 'OTH']
+    return s.st_mode & getattr(stat, 'S_I{}{}'.format(perm, pos))
+
+
+def oth_w_perm(file):
+    return bitperm(os.stat(file), 'w', 'oth')
+
+
+def only_root_write(path):
+    # http://stackoverflow.com/questions/10741580/using-pythons-stat-function-to-efficiently-get-owner-group-and-other-permissio
+    s = os.stat(path)
+    for ug, bp in [(s.st_uid, bitperm(s, 'w', 'usr')), (s.st_gid, bitperm(s, 'w', 'grp'))]:
+        # User id (is not root) and bit permission
+        if ug and bp:
+             return False
+    if bitperm(s, 'w', 'oth'):
+        return False
+    return True
+
+
 class Config(dict):
     def __init__(self, file, **kwargs):
         super().__init__(**kwargs)
+        if not (os.getuid() and only_root_write(file)) or oth_w_perm(file):
+            raise SecurityException('There should be no permissions for other users in the file "{}". {}.'.format(
+                file, 'Removes write permission for others' if os.getuid()
+                else 'Only root must be able to write to file'
+            ))
         self.file = file
         self.read()
 
