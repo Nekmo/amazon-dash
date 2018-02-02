76173203c3b2845f7526507bec17020be97193e9
Nekmo <contacto@nekmo.com>
2018-02-02 18:44:52 +0100

    Systemd install

--- amazon_dash/install/__main__.py
+++ amazon_dash/install/__main__.py
@@ -1,11 +1,28 @@
 import os
 import shutil
 
+from subprocess import check_output
+
 CONFIG_PATH = '/etc/amazon-dash.yml'
+SYSTEMD_PATHS = [
+    '/usr/lib/systemd/system',
+    '/lib/systemd/system',
+]
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
 CONFIG_EXAMPLE = os.path.join(__dir__, 'amazon-dash.yml')
+SYSTEMD_SERVICE = os.path.join(__dir__, 'services', 'amazon-dash.service')
+
+
+def get_pid(name):
+    return check_output(["pidof", name])
+
+
+def get_systemd_services_path():
+    for path in SYSTEMD_PATHS:
+        if os.path.lexists(path):
+            return path
 
 
 class InstallException(Exception):
@@ -51,3 +68,27 @@ class InstallConfig(InstallBase):
         shutil.copy(CONFIG_EXAMPLE, CONFIG_PATH)
         os.chmod(CONFIG_PATH, 0o600)
         os.chown(CONFIG_PATH, 0, 0)
+
+
+class InstallSystemd(InstallBase):
+    service_name = os.path.split(SYSTEMD_SERVICE)[-1]
+
+    @property
+    def service_path(self):
+        path = get_systemd_services_path()
+        if not path:
+            return
+        return os.path.join(path, self.service_name)
+
+    def is_installable(self):
+        if not get_pid('systemd') and get_systemd_services_path():
+            raise IsInstallableException('Systemd is not available')
+
+    def is_necessary(self):
+        if os.path.lexists(self.service_path):
+            raise IsInstallableException('Systemd is not available')
+
+    def installation(self):
+        shutil.copy(SYSTEMD_SERVICE, self.service_path)
+        os.chmod(self.service_path, 0o600)
+        os.chown(self.service_path, 0, 0)
