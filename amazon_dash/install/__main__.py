795b9a2fe521f2e61a46845370bfb05c9d28f156
Nekmo <contacto@nekmo.com>
2018-02-02 20:07:58 +0100

    Systemd install

--- amazon_dash/install/__main__.py
+++ amazon_dash/install/__main__.py
@@ -1,7 +1,12 @@
 import os
 import shutil
 
+import sys
+from click_default_group import DefaultGroup
+
 from subprocess import check_output
+
+import click as click
 
 CONFIG_PATH = '/etc/amazon-dash.yml'
 SYSTEMD_PATHS = [
@@ -25,19 +30,44 @@ def get_systemd_services_path():
             return path
 
 
+def catch(fn, exception_cls=None):
+    exception_cls = exception_cls or InstallException
+
+    def wrap(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except exception_cls as e:
+            click.echo('{}'.format(e), err=True)
+    return wrap
+
+
+def install_success(name):
+    click.echo('[OK] {} has been installed successfully'.format(name))
+    return True
+
+
 class InstallException(Exception):
-    pass
+    name = 'Install Error'
+    tpl = '[{name}] {body}'
+
+    def __init__(self, body='No details'):
+        self.body = body
+
+    def __str__(self):
+        return self.tpl.format(name=self.name, body=self.body)
 
 
 class IsInstallableException(InstallException):
-    pass
+    name = 'Unable to install'
 
 
 class IsNecessaryException(InstallException):
-    pass
+    name = 'Already installed'
 
 
 class InstallBase(object):
+    name = ''
+
     def is_installable(self):
         raise NotImplementedError
 
@@ -51,9 +81,11 @@ class InstallBase(object):
         self.is_installable()
         self.is_necessary()
         self.installation()
+        return True
 
 
 class InstallConfig(InstallBase):
+    name = 'config'
 
     def is_installable(self):
         directory = os.path.dirname(CONFIG_PATH)
@@ -71,6 +103,7 @@ class InstallConfig(InstallBase):
 
 
 class InstallSystemd(InstallBase):
+    name = 'systemd'
     service_name = os.path.split(SYSTEMD_SERVICE)[-1]
 
     @property
@@ -86,9 +119,54 @@ class InstallSystemd(InstallBase):
 
     def is_necessary(self):
         if os.path.lexists(self.service_path):
-            raise IsInstallableException('Systemd is not available')
+            raise IsNecessaryException('Systemd service is already installed')
 
     def installation(self):
         shutil.copy(SYSTEMD_SERVICE, self.service_path)
         os.chmod(self.service_path, 0o600)
         os.chown(self.service_path, 0, 0)
+
+
+SERVICES = [
+    InstallSystemd,
+]
+
+
+@click.group(cls=DefaultGroup, default='all', default_if_no_args=True)
+def cli():
+    if os.getuid():
+        click.echo('The installation must be done as root. Maybe you forgot sudo?', err=True)
+        sys.exit(1)
+
+
+@catch
+@cli.command()
+def config():
+    InstallConfig().install() and install_success('config')
+
+
+@catch
+@cli.command()
+def systemd():
+    InstallSystemd().install() and install_success('systemd service')
+
+
+@cli.command()
+def all():
+    click.echo('Executing all install scripts for Amazon-Dash')
+    catch(config)()
+    has_service = False
+    for service in SERVICES:
+        try:
+            has_service = has_service or (service().install() and
+                                          install_success('{} service'.format(service.name)))
+        except IsInstallableException:
+            pass
+        except IsNecessaryException as e:
+            has_service = True
+            click.echo('{}'.format(e), err=True)
+    if not has_service:
+        click.echo('Warning: There is no service installed in the system. You must run Amazon-dash manually')
+
+
+catch(cli)()
