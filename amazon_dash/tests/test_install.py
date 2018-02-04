e314e60058d4c744ffffd8cefe32bc703752fc97
Nekmo <contacto@nekmo.com>
2018-02-04 17:44:48 +0100

    TestInstallConfig

--- amazon_dash/tests/test_install.py
+++ amazon_dash/tests/test_install.py
@@ -1,8 +1,9 @@
 import unittest
 
+import os
 from pyfakefs.fake_filesystem_unittest import Patcher
 
-from amazon_dash.install import InstallConfig, IsInstallableException
+from amazon_dash.install import InstallConfig, IsInstallableException, CONFIG_PATH, IsNecessaryException, CONFIG_EXAMPLE
 
 
 class TestInstallConfig(unittest.TestCase):
@@ -10,4 +11,22 @@ class TestInstallConfig(unittest.TestCase):
         with Patcher() as patcher:
             with self.assertRaises(IsInstallableException):
                 InstallConfig().is_installable()
-            # patcher.fs.create_file('/foo/bar', contents='test')
+            patcher.fs.CreateFile(CONFIG_PATH)
+            InstallConfig().is_installable()
+
+    def test_is_necessary(self):
+        with Patcher() as patcher:
+            InstallConfig().is_necessary()
+            patcher.fs.CreateFile(CONFIG_PATH)
+            with self.assertRaises(IsNecessaryException):
+                InstallConfig().is_necessary()
+
+    def test_installation(self):
+        with Patcher() as patcher:
+            os.makedirs(os.path.dirname(CONFIG_PATH))
+            patcher.fs.CreateFile(CONFIG_EXAMPLE)
+            InstallConfig().installation()
+            stat = os.stat(CONFIG_PATH)
+            self.assertEqual(stat.st_mode, 0o100600)
+            self.assertEqual(stat.st_uid, 0)
+            self.assertEqual(stat.st_gid, 0)
