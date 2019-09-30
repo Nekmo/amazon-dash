44f6abf3a4a4a80f3bda50e12c1ff773c0f94acf
Nekmo <contacto@nekmo.com>
2019-10-01 00:58:29 +0200

    Issue #145: Amazon abandoned Dash Buttons (get info from button).

--- amazon_dash/wifi.py
+++ amazon_dash/wifi.py
@@ -4,7 +4,12 @@ iwconfig wlan0 essid NETWORK_NAME key WIRELESS_KEY
 dhclient wlan0
 """
 import subprocess
+from bs4 import BeautifulSoup
 
+import requests
+
+
+CONFIGURE_URL = 'http://192.168.0.1/'
 
 
 def get_cmd_output(cmd, split_lines=True, decode='utf-8'):
@@ -16,7 +21,7 @@ def get_cmd_output(cmd, split_lines=True, decode='utf-8'):
     return output
 
 
-class Wifi:
+class Wifi(object):
 
     def __init__(self, device=None):
         self.device = device or next(self.get_wireless_devices(), None)
@@ -39,6 +44,24 @@ class Wifi:
         get_cmd_output(['dhclient', self.device])
 
 
+class ConfigureAmazonDash(object):
+    def __init__(self):
+        pass
+
+    def get_info(self):
+        r = requests.get(CONFIGURE_URL)
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, 'html.parser')
+        items = soup.find_all('td')
+        return {
+            'serial_number': items[0].string,
+            'mac_address': items[1].string,
+            'firmware': items[2].string,
+            'battery': items[3].string,
+        }
+
+
 if __name__ == '__main__':
     w = Wifi()
     w.connect('Amazon ConfigureMe')
+    w.dhcp()
