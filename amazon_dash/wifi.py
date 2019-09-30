edde55a1b2baef93b99c38a8e2088347411179be
Nekmo <contacto@nekmo.com>
2019-10-01 00:18:41 +0200

    Issue #145: Amazon abandoned Dash Buttons (discover wireless devices).

--- amazon_dash/wifi.py
+++ amazon_dash/wifi.py
@@ -3,3 +3,18 @@ netstat -i
 iwconfig wlan0 essid NETWORK_NAME key WIRELESS_KEY
 dhclient wlan0
 """
+import subprocess
+
+
+def get_cmd_output(cmd, split_lines=True):
+    output = subprocess.check_output(cmd)
+    if split_lines:
+        output = [line.rstrip('\n') for line in output.split('\n')]
+    return output
+
+
+class Wifi:
+
+    def get_wireless_devices(self):
+        devices = get_cmd_output(['netstat', '-i'])[2:]
+        return map(lambda x: x.split(' ')[0], filter(lambda x: x.startswith('wl'), devices))
