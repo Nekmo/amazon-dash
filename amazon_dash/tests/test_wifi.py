abe0f659e92759e1a9db711e7ecd6f0b3f7d5cd3
Nekmo <contacto@nekmo.com>
2020-04-11 02:04:08 +0200

    Issue #145: Amazon abandoned Dash Buttons (TestConfigureAmazonDash)

--- amazon_dash/tests/test_wifi.py
+++ amazon_dash/tests/test_wifi.py
@@ -1,8 +1,10 @@
 import unittest
 from unittest.mock import patch
 
+import requests_mock
+
 from amazon_dash.exceptions import ConfigWifiError
-from amazon_dash.wifi import Wifi, NetworkManagerWifi
+from amazon_dash.wifi import Wifi, NetworkManagerWifi, ConfigureAmazonDash, CONFIGURE_URL
 
 DEVICES_OUTPUT = """
 1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
@@ -26,6 +28,40 @@ WIRELESS_DEVICES_OUTPUT = """
     inet6 aaaa::bbbb:cccc:dddd:eeee/64 scope link noprefixroute 
        valid_lft forever preferred_lft forever
 """
+AMAZON_DASH_RESPONSE = """
+<tr class="row">
+<th>Serial Number</th>
+<td>G010M80234000000</td>
+</tr>
+<tr class="row">
+<th>MAC Address</th>
+<td>AABBCCDDEEFF</td>
+</tr>
+<tr class="row">
+<th>Firmware</th>
+<td>30017420_EU</td>
+</tr>
+<tr class="row">
+<th>Battery</th>
+<td>30</td>
+</tr>
+"""
+NETWORKS_RESPONSE = {
+   "amzn_devid":"G010M80234000000",
+   "amzn_macid":"",
+   "international":1,
+   "amzn_networks":[
+      {
+         "ssid":"wifi-eggs",
+         "bssid":"",
+         "security":"WPA AES PSK",
+         "rssi":"-92"
+      }
+   ],
+   "schemes":[
+      0
+   ]
+}
 
 
 class TestWifi(unittest.TestCase):
@@ -76,3 +112,40 @@ class TestNetworkManagerWifi(unittest.TestCase):
         wifi = NetworkManagerWifi(device)
         wifi.connect(essid, key)
         m.assert_called_once_with(['nmcli', 'device', 'wifi', 'connect', essid, 'password', key])
+
+
+class TestConfigureAmazonDash(unittest.TestCase):
+    def test_get_info(self):
+        with requests_mock.mock() as m:
+            m.get(CONFIGURE_URL, text=AMAZON_DASH_RESPONSE)
+            self.assertEqual(ConfigureAmazonDash().get_info(), {
+                'serial_number': 'G010M80234000000',
+                'mac_address': 'AABBCCDDEEFF',
+                'firmware': '30017420_EU',
+                'battery': '30',
+            })
+
+    def test_get_networks_availables(self):
+        with requests_mock.mock() as m:
+            m.get(CONFIGURE_URL, json=NETWORKS_RESPONSE,
+                  headers={'Content-Type': 'application/json'})
+            self.assertEqual(list(ConfigureAmazonDash().get_networks_availables()),
+                             NETWORKS_RESPONSE['amzn_networks'])
+
+    @requests_mock.mock()
+    def test_configure_error(self, m):
+        m.get(CONFIGURE_URL, json=NETWORKS_RESPONSE,
+              headers={'Content-Type': 'application/json'})
+
+        with self.assertRaises(ConfigWifiError):
+            ConfigureAmazonDash().configure('foo', 'bar')
+
+    @requests_mock.mock()
+    def test_configure(self, m):
+        amzn_ssid = 'wifi-eggs'
+        amzn_pw = 'bar'
+        m.get(CONFIGURE_URL, json=NETWORKS_RESPONSE,
+              headers={'Content-Type': 'application/json'})
+        m.get(CONFIGURE_URL + '?amzn_ssid={}&amzn_pw={}'.format(amzn_ssid, amzn_pw),
+              json=NETWORKS_RESPONSE)
+        ConfigureAmazonDash().configure(amzn_ssid, amzn_pw)
